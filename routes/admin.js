1035defcb9b4fb95514170ec545cc7ae50b05e18
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-03-28 22:54:09 -0300

    refactor: update route name

--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ router.get("/edit-product/:id", isAuth, adminController.getEditProduct);
 
 router.post("/edit-product", isAuth, postEditProductValidation(), adminController.postEditProduct);
 
-router.post("/delete-product", isAuth, adminController.postDeleteProduct)
+router.delete("/product/:productId", isAuth, adminController.deleteProduct)
 
 module.exports = router;
 
