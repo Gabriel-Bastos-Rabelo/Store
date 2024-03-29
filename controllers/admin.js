db5c45b5493da98f4750dcf9c6b8bba97f074e4f
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-03-28 22:46:07 -0300

    refactor: update deleteProduct function to use params and return json

--- controllers/admin.js
+++ controllers/admin.js
@@ -146,61 +146,54 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 
-exports.postEditProduct = (req, res, next) => {
-
-  const id = req.body.productId;
-  const title = req.body.title;
+exports.postEditProduct = async (req, res, next) => {
+  const { productId: id, title, description, price } = req.body;
   const image = req.file;
-  const description = req.body.description;
-  const price = req.body.price;
-  const errors = validationResult(req)
-
+  const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-
-    return res.render('admin/edit-product', {
+    return res.status(422).render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
-      product: { id, title, description, price },
       editing: true,
       isLoggedIn: req.session.isLoggedIn,
-      errorMessage: errors.array()[0].msg
-
-
+      product: { id, title, description, price },
+      errorMessage: errors.array()[0].msg,
     });
   }
 
-  let productUpdated = {}
-  productUpdated.title = title;
-  productUpdated.price = price;
-  if (image) {
-    deleteFile(productUpdated.imageUrl);
-    productUpdated.imageUrl = image.path;
-  }
-  productUpdated.description = description;
-
-  console.log(productUpdated);
-  
-
-  Product.update(productUpdated, {
-    where: {
-      id: id,
-      userId: req.session.user.id
+  try {
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return next(new Error("Product not found"));
     }
-  }).then(() => {
+
+    if (product.userId !== req.session.user.id) {
+      return res.redirect('/');
+    }
+
+    if (image) {
+      deleteFile(product.imageUrl);
+      product.imageUrl = image.path;
+    }
+
+    product.title = title;
+    product.price = price;
+    product.description = description;
+
+    await product.save();
     res.redirect("/admin/products");
-  }).catch(err => {
-    const error = new Error("Post edit product failed");
+  } catch (err) {
+    const error = new Error("Editing product failed");
     error.httpStatusCode = 500;
     return next(error);
-  })
+  }
+};
 
 
-}
+exports.deleteProduct = (req, res, next) => {
 
-exports.postDeleteProduct = (req, res, next) => {
-
-  const id = req.body.productId;
+  const id = req.params.productId;
   const userId = req.session.user.id;
 
   Product.findByPk(id).then(product => {
@@ -218,11 +211,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
   })
   .then(() => {
-    res.redirect("/admin/products");
+    res.status(200).json({message: 'Success!'});
   }).catch(err => {
-    const error = new Error("Post delete product failed");
-    error.httpStatusCode = 500;
-    return next(error);
+    res.status(500).json({message: 'Failed to delete product'});
   })
 
 
